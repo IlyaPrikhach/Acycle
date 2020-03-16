3de201c8d075f342c7a0df0d316c3506a5a0b1ba
IlyaPrikhach <61990763+IlyaPrikhach@users.noreply.github.com>
2020-03-16 20:28:02 +0300

    Add files via upload

--- Ациклы9/Task2Epam/Program.cs
+++ Ациклы9/Task2Epam/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace test
 {
-  
+
     class Program
     {
         static void Main(string[] args)
@@ -17,7 +17,7 @@ namespace test
             {
                 Console.WriteLine("Введите k и x");
                 string строкаВвода = Console.ReadLine();
-                string[] аргумент = строкаВвода.Split(' ');
+                string[] аргумент = строкаВвода.Replace('.', ',').Split(' ');
                 k = Convert.ToInt16(аргумент[0]);
                 x = Convert.ToDouble(аргумент[1]);
             }
@@ -29,13 +29,13 @@ namespace test
             double слагаемое = 0, сумма = слагаемое;
             for (int n = 1; n <= k; n++)
             {
-                
+
                 слагаемое = Math.Pow(-1, n + 1) * Math.Pow(x, 2 * n + 1) / (4 * Math.Pow(n, 2) - 1);
                 сумма += слагаемое;
             }
             Console.Write(сумма);
             Console.ReadLine();
-            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
         }
     }
 }
